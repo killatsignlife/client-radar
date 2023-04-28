791b49a0b1f3dc6aef0ceb14ec36db7eeeae5b70
Leonardo <91156801+Leohgb@users.noreply.github.com>
2023-04-28 20:10:25 -0300

    a

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  submitted = false;
 
-  constructor() { }
+  constructor(private service: ApiService, private router: Router) { }
 
   ngOnInit(): void {
   }
