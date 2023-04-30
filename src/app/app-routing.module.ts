2d7aef6f91a8c5af6e8f05d9456c23f9a64e508e
Leonardo <91156801+Leohgb@users.noreply.github.com>
2023-04-30 12:58:40 -0300

    add-voluntario.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './views/login/login.component';
 import { DeleteDesaparecidoComponent } from './desaparecidos/delete-desaparecido/delete-desaparecido.component';
 import { UpdateDesaparecidoComponent } from './desaparecidos/update-desaparecido/update-desaparecido.component';
 import { ListVoluntarioComponent } from './voluntarios/list-voluntario/list-voluntario.component';
+import { AddVoluntarioComponent } from './voluntarios/add-voluntario/add-voluntario.component';
 import { DeleteVoluntarioComponent } from './voluntarios/delete-voluntario/delete-voluntario.component';
 import { UpdateVoluntarioComponent } from './voluntarios/update-voluntario/update-voluntario.component';
 import { AddDesaparecidoComponent } from './desaparecidos/add-desaparecido/add-desaparecido.component';
@@ -27,18 +28,20 @@ const routes: Routes = [
   { path: 'reportar/:id', component: ReportarComponent },
   { path: 'agradecimento', component: AgradecimentosComponent },
   { path: 'acessibilidade', component: AcessibilidadeComponent },
-  { path:'desaparecidos', component: DesaparecidosComponent},
-  { path:'descricao/:id', component: DescricaoComponent},
+  { path: 'desaparecidos', component: DesaparecidosComponent},
+  { path: 'descricao/:id', component: DescricaoComponent},
   { path: 'sucesso', component: SucessoComponent},
-  { path:'blogs', component: AllBlogsComponent},
-  { path:'blog', component: BlogComponent},
-  { path:'login', component: LoginComponent},
+  { path: 'blogs', component: AllBlogsComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'desaparecido/deletar/:id', component: DeleteDesaparecidoComponent},
   { path: 'desaparecido/atualizar/:id', component: UpdateDesaparecidoComponent},
   { path: 'desaparecido/adicionar', component: AddDesaparecidoComponent},
   { path: 'voluntarios', component: ListVoluntarioComponent},
   { path: 'voluntarios/deletar/:id', component: DeleteVoluntarioComponent},
-  { path: 'voluntarios/atualizar/:id', component: UpdateVoluntarioComponent}
+  { path: 'voluntarios/atualizar/:id', component: UpdateVoluntarioComponent},
+  { path: 'voluntarios/adicionar', component: AddVoluntarioComponent},
+
 ];
 
 @NgModule({
