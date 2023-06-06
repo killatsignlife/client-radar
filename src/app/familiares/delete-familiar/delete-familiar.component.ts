c70ff38d21ab1f91bb311833188667701166c39f
Pablo Flores <120192554+PACFWL@users.noreply.github.com>
2023-06-05 22:30:17 -0300

    ↑ delete Familiar

--- src/app/familiares/delete-familiar/delete-familiar.component.ts
+++ src/app/familiares/delete-familiar/delete-familiar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Familiar } from 'src/app/familiar.model';
+import Swal from 'sweetalert2';
+
+
+
 
 @Component({
   selector: 'app-delete-familiar',
@@ -7,9 +14,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteFamiliarComponent implements OnInit {
 
-  constructor() { }
+   // @ts-ignore: Object is possibly 'undefined'.
+  familiar: Familiar;
+  // @ts-ignore: Object is possibly 'undefined'.
+  familiarId: number;
 
-  ngOnInit(): void {
+  constructor(private api: ApiService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit()  {
+
+    this.familiar = new Familiar();
+      this.familiarId = this.route.snapshot.params['id'];
+  
+      this.api.getFamiliarById(this.familiarId)
+        .subscribe(data => {
+          console.log(data);
+          this.familiar = data;
+        }, error => console.log(error));
   }
 
+
+  deleteFamiliar(): void {
+    this.api.deleteFamiliar(this.familiarId).subscribe(() => {
+      this.router.navigate(["familiar/list"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['familiar/list']);
+  }
+
+  popup(){
+    Swal.fire({
+      title: 'Tem certeza que deseja excluir este cadastro?',
+      icon: 'warning',
+      confirmButtonColor: '#56c865',
+      cancelButtonColor: '#d33',
+      cancelButtonText:'Não',
+      confirmButtonText: 'Sim',
+      showCancelButton: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: 'success',
+          confirmButtonColor: '#56c865',
+          text: 'Cadastro excluído',
+          confirmButtonText: 'ok',
+        })
+        this.deleteFamiliar() 
+      }
+    })
+  }
+
+  
 }
