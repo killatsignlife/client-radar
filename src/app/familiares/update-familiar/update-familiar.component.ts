008a91c0a4fed4b3bab6c6c36a7a7e3eeb922a39
Pablo Flores <120192554+PACFWL@users.noreply.github.com>
2023-06-05 22:38:13 -0300

    ↑ Update Familiar

--- src/app/familiares/update-familiar/update-familiar.component.ts
+++ src/app/familiares/update-familiar/update-familiar.component.ts
@@ -1,4 +1,11 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Familiar } from 'src/app/familiar.model';
+import { SharedDataService } from 'src/app/shared-data.service';
+
+
 
 @Component({
   selector: 'app-update-familiar',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateFamiliarComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore: Object is possibly 'undefined'.
+  familiar: Familiar;
+  // @ts-ignore: Object is possibly 'undefined'.
+  familiarId: number;
 
-  ngOnInit(): void {
+  constructor(private api: ApiService, private route: ActivatedRoute, private router: Router
+    ,private maxLength: SharedDataService,  private cepService: SharedDataService) { }
+
+  ngOnInit() {
+    this.maxLength.maxCaracteres();
+    this.familiar = new Familiar();
+      this.familiarId = this.route.snapshot.params['id'];
+  
+      this.api.getFamiliarById(this.familiarId)
+        .subscribe(data => {
+          console.log(data);
+          this.familiar = data;
+        }, error => console.log(error));
+  }
+
+  updateFamiliar() {
+    this.api.updateFamiliar(this.familiarId, this.familiar)
+      .subscribe(data => console.log(data), error => console.log(error));
+      this.familiar = new Familiar();
+
+      this.router.navigate(['familiar/list']);
+  }
+
+  onSubmit() {
+    this.updateFamiliar();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    return filterValue;
+  }
+
+  consultaCep(val, form){
+    this.cepService.buscar(val).subscribe((dados) => this.populaForm(dados, form));
+  }
+
+  populaForm(dados, form){
+    form.controls['cidade'].setValue(dados.localidade)
+    form.controls['bairro'].setValue(dados.bairro)
+    form.controls['logradouro'].setValue(dados.logradouro)
+    form.controls['uf'].setValue(dados.uf)
   }
 
 }
